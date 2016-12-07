b5efd41ed3749fe1909bc2841ad0bbda32c6b7b0
atmaxinger <maxi6594@gmail.com>
2016-12-07 15:31:27 +0100

    added nunit

---
+++ tctest/tests/SomethingTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit;
+using NUnit.Framework;
+
+namespace tctest.tests
+{
+	[TestFixture]
+	class SomethingTest
+	{
+		[Test]
+		public void SomeWorkingTest()
+		{
+			Assert.That(true);
+		}
+
+		[Test]
+		public void SomeFailingTest()
+		{
+			Assert.That(false);
+		}
+	}
+}
